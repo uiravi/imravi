747eadcffb476c0196538d05b54b2e1ecf7ee964
Ravi Shankar Kumar <ravishankar.kumar@bold.com>
2020-05-31 23:25:27 +0530

    Testing the changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        <p>Testing the changes</p>
       </header>
     </div>
   );
