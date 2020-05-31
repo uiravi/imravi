85bd8c6c9ee9036d8e5b99c264e2a0dc9f92e955
Ravi Shankar Kumar <ravishankar.kumar@bold.com>
2020-05-31 23:06:53 +0530

    testing changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
-        <p>Hosting website is very easy with github pages</p>
       </header>
     </div>
   );
