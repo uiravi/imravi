598b52b7875ff982cca5073465145770418172cb
Ravi Shankar Kumar <ravishankar.kumar@bold.com>
2020-06-01 11:47:43 +0530

    First commit after resolving domain overwritten issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import AnimatedBack from "./components/AnimatedBack";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+export default class App extends PureComponent {
+  render(){
+    return (
+      <React.Fragment>
+          <AnimatedBack />
+          <div>Hello World</div>
+      
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/AnimatedBack.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default function AnimatedBack(){
+    return(
+        <React.Fragment>
+            <div id='stars'></div>
+            <div id='stars2'></div>
+            <div id='stars3'></div>
+        </React.Fragment>
+    )
+
+    
+}
