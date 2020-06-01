5a817925a0b9c0816eec97929cfb9293b9dca04f
Ravi Shankar Kumar <ravishankar.kumar@bold.com>
2020-06-01 09:28:40 +0530

    Updates

--- precache-manifest.e992f781f12ff023379a76d19e89d4f9.js
+++ precache-manifest.e992f781f12ff023379a76d19e89d4f9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bfe9bf47b40922e3c74052af58e0e663",
+    "revision": "d9f6d85cfd527431c6fb95db9274bd4c",
     "url": "/index.html"
   },
   {
-    "revision": "cd57bc8f72e32cbf65ee",
+    "revision": "0dcaa6f0ecb780aac41b",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.7b544435.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cd57bc8f72e32cbf65ee",
-    "url": "/static/js/main.9ef0806a.chunk.js"
+    "revision": "0dcaa6f0ecb780aac41b",
+    "url": "/static/js/main.d09b196b.chunk.js"
   },
   {
     "revision": "b59e901a49245db03ad3",
